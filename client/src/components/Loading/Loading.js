e95c5dc032d68435baef31fd2728b0ae62a85ffc
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2020-10-10 09:27:10 +0100

    authentication: initial Loading component

---
+++ client/src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div>
+      <a href="https://github.com/login/oauth/authorize"></a>
+    </div>
+  );
+}
+
+export default Loading;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
+import Loading from "../Loading/Loading";
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1 className="text-danger border-white">Elit</h1>
-      <button type="button" className="btn btn-danger">Github login/signup</button>
+      <Loading />
     </nav>
-    
   );
 };
 
