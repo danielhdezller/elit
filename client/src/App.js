ce2c3febe0ee1dac77a74cff44a200dbdb4e40da
Katarzyna <kolnykatarzyna@gmail.com>
2020-10-10 19:24:30 +0100

    Feat: Working on dashboard routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
-import Dashboard from './components/Dashboard/Dashboard';
 import Home from './components/Home/Home';
-import Profile from './components/Profile/Profile';
+import Dashboard from './components/Dashboard/Dashboard';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import users from './mockData';
 import UserProfile from './components/UserProfile/UserProfile';
@@ -15,12 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' render={() => <Home users={users} />} />
         <Route exact path='/dashboard' component={Dashboard} />
-        <Route exact path='/profile' component={Profile} />
-        <Route
-          exact
-          path='/members/:name'
-          component={UserProfile}
-          render={() => <UserProfile users={users} />}
+        <Route exact path='/members/:name' render={({match}) => <UserProfile match={match} users={users} />}
         />
       </Switch>
     </Router>
--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -2,20 +2,16 @@ import React from 'react';
 
 const UserProfile = ({ match, users }) => {
   const name = match.params.name;
-
-  users.map((user) => {
-    if (user.userName === name) {
-      return (
-        <div>
-          <h1>
-            {user.name} {user.familyName}
-          </h1>
-          hi {name}!<h3>{user.userName}</h3>
-          {usersProfile}
-          <h5>{user.techStack}</h5>
-        </div>
-      );
-    }
-  });
+  const firstName = users.map((user) => user.userName === name ? user.name : null)
+  const lastName = users.map((lastname) => lastname.userName === name ? lastname.familyName : null)
+  const stacks = users.map((username) => username.userName === name ? username.techStack : null)
+  
+  return (
+    <>
+    <h1>{firstName}</h1>
+    <h1>{lastName}</h1>
+    <h1>{stacks}</h1>
+    </>
+  );
 };
 export default UserProfile;
