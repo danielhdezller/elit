29249424e79c67fa7a6dfd1ab1f73e7b26f2ccff
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2020-10-14 21:35:04 +0100

    feat: navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
-import Login from '../Login/Login';
-=======
-import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import Login from '../Login/Login'
+import Login from '../Login/Login';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -25,38 +21,32 @@ const useStyles = makeStyles((theme) => ({
   },
   bg: {
     backgroundColor: '#000',
-  }
+  },
 }));
->>>>>>> 60995b3ea984c62f3529605b9a3c6a6e1be95549
 
 const Navbar = () => {
-
   const classes = useStyles();
 
   return (
-<<<<<<< HEAD
-    <nav className='navbar bg-dark'>
-      <h1 className='text-danger border-white'>Elit</h1>
-      <Login />
-    </nav>
-  );
-};
-=======
     <div className={classes.root}>
-      <AppBar position="static" className={classes.bg}>
+      <AppBar position='static' className={classes.bg}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h4" className={classes.title}>
+          <Typography variant='h4' className={classes.title}>
             Elit
           </Typography>
-            <Login  />
+          <Login />
         </Toolbar>
       </AppBar>
     </div>
-  )
-}
->>>>>>> 60995b3ea984c62f3529605b9a3c6a6e1be95549
+  );
+};
 
 export default Navbar;
