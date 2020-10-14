010f00c98df045bbad2e47571474622668bf34a6
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2020-10-14 23:44:36 +0100

    fixed: worong files merged by error, dev works now

--- server/index.js
+++ server/index.js
@@ -3,10 +3,6 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./GraphQL/graphQlSchemas');
 const resolvers = require('./GraphQL/resolvers');
 
-const { createStore } = require('./utils');
-
-const store = createStore();
-
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
