f79f7f3bf706db34cc97e0e327d1fbeac2d92ebe
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2020-10-14 21:28:37 +0100

    fix: store

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./GraphQL/graphQlSchemas');
 const resolvers = require('./GraphQL/resolvers');
 
-const { createStore } = require('./utils');
+// const { createStore } = require('./utils');
 
-const store = createStore();
+// const store = createStore();
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
