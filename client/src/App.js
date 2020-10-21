5f32a5ca6702ccfd65162f0df1e39ce29e4ebc16
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2020-10-21 18:27:44 +0100

    feat: commets deleted

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,6 @@ function App() {
           <Route
             exact
             path='/member/:name/events'
-            // render={() => <CommunityEvents events={} />}
             component={CommunityEvents}
           />
           <Route exact path='/member/:name/myevents' render={MyEvents} />
